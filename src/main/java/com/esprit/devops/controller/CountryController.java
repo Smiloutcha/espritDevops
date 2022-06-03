2f4d4e721c74a922df705011a3d00809c2b4390d
Lakhdhar Med Akrem <54031336+Smiloutcha@users.noreply.github.com>
2022-06-04 00:00:33 +0200

    Update CountryController.java

--- src/main/java/com/esprit/devops/controller/CountryController.java
+++ src/main/java/com/esprit/devops/controller/CountryController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-@RestController("/countries")
+@RestController
+@RequestMapping("/countries")
 @AllArgsConstructor
 public class CountryController {
 
