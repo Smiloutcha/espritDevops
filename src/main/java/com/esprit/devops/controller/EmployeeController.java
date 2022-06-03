99b78ad4f7dfce3da47d6749c2254bf39e6565a4
Lakhdhar Med Akrem <54031336+Smiloutcha@users.noreply.github.com>
2022-06-04 00:01:41 +0200

    Update EmployeeController.java
    Fix Commit

--- src/main/java/com/esprit/devops/controller/EmployeeController.java
+++ src/main/java/com/esprit/devops/controller/EmployeeController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-@RestController("/employees")
+@RestController
+@RequestMapping("/employees")
 @AllArgsConstructor
 public class EmployeeController {
 
