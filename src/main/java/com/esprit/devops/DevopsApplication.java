0ba6963399d25cc9f1ef258abe781045ad5725ee
Lakhdhar Med Akrem <54031336+Smiloutcha@users.noreply.github.com>
2022-06-04 08:06:30 +0200

    Log when starting the application

--- src/main/java/com/esprit/devops/DevopsApplication.java
+++ src/main/java/com/esprit/devops/DevopsApplication.java
@@ -1,13 +1,19 @@
 package com.esprit.devops;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevopsApplication {
 
+	private static final Logger logger = LogManager.getLogger();
+
 	public static void main(String[] args) {
+		logger.info("Starting the application ================");
 		SpringApplication.run(DevopsApplication.class, args);
+		logger.info("Done starting the application DevopsApplication");
 	}
 
 }
