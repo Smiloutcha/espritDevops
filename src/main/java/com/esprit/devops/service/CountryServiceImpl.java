ea517044e714d87b77489b34ffc90628875f8f25
Lakhdhar Med Akrem <54031336+Smiloutcha@users.noreply.github.com>
2022-06-04 08:07:14 +0200

    Add logging messages

--- src/main/java/com/esprit/devops/service/CountryServiceImpl.java
+++ src/main/java/com/esprit/devops/service/CountryServiceImpl.java
@@ -3,28 +3,40 @@ package com.esprit.devops.service;
 import com.esprit.devops.model.Country;
 import com.esprit.devops.repository.CountryRepository;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class CountryServiceImpl implements  CountryService{
 
+
     private final CountryRepository countryRepository;
+    private static final Logger logger  = LogManager.getLogger() ;
 
     @Override
     public Country save(Country country) {
-       return countryRepository.save(country);
+        logger.info("Start Saving Country");
+       Country savedCountry = countryRepository.save(country);
+       logger.info("End Sacing Country");
+       return savedCountry ;
     }
 
     @Override
     public Collection<Country> findAll() {
-       return countryRepository.findAll();
+        logger.info("Start Fetching All countries");
+        List<Country> allCountries = countryRepository.findAll();
+        logger.info("End Fetching All Countries");
+        return allCountries ;
     }
 
     @Override
     public Country findById(String id) {
+        logger.info("Start find by Id process");
        return countryRepository.findById(id).orElse(null);
     }
 
@@ -35,12 +47,15 @@ public class CountryServiceImpl implements  CountryService{
             countryRepository.deleteById(id);
             return countryToremove ;
         } else {
+            logger.error("Country does not exist in the database");
             throw new RuntimeException("Hello");
         }
     }
 
     @Override
     public void deleteAll() {
+        logger.info("Start Deleting all countries");
         countryRepository.deleteAll();
+        logger.info("end deleting all countries");
     }
 }
