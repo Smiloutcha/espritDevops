fa5d303c544237dc97e47c7da4900fe0ea046db1
Lakhdhar Med Akrem <54031336+Smiloutcha@users.noreply.github.com>
2022-06-04 09:13:41 +0200

    Update Employee.java

--- src/main/java/com/esprit/devops/model/Employee.java
+++ src/main/java/com/esprit/devops/model/Employee.java
@@ -29,4 +29,44 @@ public class Employee implements Serializable {
     @ManyToOne
     private Department department;
 
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
